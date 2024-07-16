772950e34847a49d214b188285569636f33f3607
helonely <idravil1211@gmail.com>
2024-07-16 20:40:53 +0500

    fix views.py

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
-def home(request):
-    return render(request, template_name='home.html')
+def list_products(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, template_name='product_list.html', context=context)
 
 
-def contacts(request):
-    return render(request, template_name='contacts.html')
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, template_name='product_detail.html', context=context)
+
