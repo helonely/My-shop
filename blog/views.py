fa8d0041a3081c9e9822fb8a259f85ec66f66234
helonely <idravil1211@gmail.com>
2024-07-23 03:52:58 +0500

    add and create views

---
+++ blog/views.py
@@ -0,0 +1,52 @@
+from django.urls import reverse_lazy
+from django.utils.text import slugify
+from django.views.generic import CreateView, ListView, DetailView, UpdateView, DeleteView
+
+from blog.models import Blog
+
+
+class BlogCreateView(CreateView):
+    model = Blog
+    fields = ('title', 'slug', 'content', 'preview', 'is_active', 'count_views')
+    success_url = reverse_lazy('blog:blog_list')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_blog = form.save()
+            new_blog.slug = slugify(new_blog.title)
+            new_blog.save()
+        return super().form_valid(form)
+
+
+class BlogListView(ListView):
+    model = Blog
+    template_name = 'blog/blog_list.html'
+
+
+class BlogDetailView(DetailView):
+    model = Blog
+    template_name = 'blog/blog_detail.html'
+
+    def get_object(self, queryset=None):
+        self.object = super().get_object(queryset)
+        self.object.count_views += 1
+        self.object.save()
+        return self.object
+
+
+class BlogUpdateView(UpdateView):
+    model = Blog
+    fields = ('title', 'slug', 'content', 'preview', 'is_active', 'count_views')
+    template_name = 'blog/blog_form.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self):
+        return reverse_lazy('blog:blog_detail', args=[self.kwargs.get('pk')])
+
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    template_name = 'blog/blog_confirm_delete.html'
+    success_url = reverse_lazy('blog:blog_list')
+
+
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
+from django.views import View
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
@@ -42,3 +44,19 @@ class ProductDeleteView(DeleteView):
     model = Product
     template_name = 'products/product_confirm_delete.html'
     success_url = reverse_lazy('catalog:list_products')
+
+
+class ContactsView(View):
+    template_name = 'products/contacts.html'
+
+    def get(self, request, *args, **kwargs):
+        # GET-запрос
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        name = request.POST.get('name')
+        message = request.POST.get('message')
+        phone = request.POST.get('phone')
+        print(name, message, phone)
+        # возвращаем пользователю ту же страницу с формой
+        return render(request, self.template_name)
