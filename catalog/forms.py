8747d713e9046453ae0bd37b54cd573c77c7cc5d
helonely <idravil1211@gmail.com>
2024-08-09 02:31:45 +0500

    create forms.py

---
+++ catalog/forms.py
@@ -0,0 +1,46 @@
+from django import forms
+from django.forms import ModelForm, BooleanField
+
+from catalog.models import Product, Version
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            if isinstance(field, BooleanField):
+                field.widget.attrs['class'] = 'form-check-input'  # По умолчанию поле будет в виде чекбокса.
+            else:
+                field.widget.attrs['class'] = 'form-control'
+
+
+class ProductForm(StyleFormMixin, ModelForm):
+    class Meta:  # Класс формы для модели Product.
+        model = Product  # Модель для которой формируется форма.
+        exclude = ('views_counter',)
+
+    def clean_name(self):
+        cleaned_data = self.cleaned_data.get('name')
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',]
+
+        if cleaned_data.lower() in forbidden_words:
+            raise forms.ValidationError('Название содержит запрещенные слова.')
+        return cleaned_data
+
+    def clean_description(self):
+        cleaned_data = self.cleaned_data.get('description')
+        forbidden_words = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция',]
+
+        if cleaned_data.lower() in forbidden_words:
+            raise forms.ValidationError('Описание содержит запрещенные слова.')
+        return cleaned_data
+
+
+class VersionForm(StyleFormMixin, ModelForm):
+
+    class Meta:
+        model = Version
+        fields = '__all__'
+        widgets = {
+            'is_active': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
